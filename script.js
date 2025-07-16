df118e0e641161f30e8edf62046c8b86e132de9d
cheforgodwin <cheforgodwin01@gmail.com>
2025-07-16 16:55:35 -0700

    sender

---
+++ script.js
@@ -0,0 +1,16 @@
+  
+                function sendMessage(){
+                    const input = document.getElementById("input");
+                    const button = document.getElementById("button");
+
+                    // button.addEventListener("click");
+                    const message = input.value;
+                    if (message) {
+                        document.body.innerHTML += `<p>${message}</p>`;
+                        input.value = ''; // Clear the input field
+                    } else {
+                        alert("Please enter a message.");
+                    }
+                    console.log(message)
+                }
+            
